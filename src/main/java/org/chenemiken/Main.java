3d73735e141953cbe01dd7970da5e0eb6380e255
ChenemiKen <chenemiken15@gmail.com>
2024-08-26 00:48:29 +0100

    first-commit

---
+++ src/main/java/org/chenemiken/Main.java
@@ -0,0 +1,11 @@
+package org.chenemiken;
+
+import org.jpos.q2.Q2;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Q2 q2 = new Q2();
+        q2.start();
+    }
+}
